6a567d17332091d948fdcb2bcf06af57489ab199
Vinhlee95 <lethanhvinh95@gmail.com>
2018-09-11 22:45:23 +0300

    fix

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const mongoose = require('mongoose');
 const app = express();
 schema.applyMiddleware({ app });
 
-mongoose.connect("mongodb://vinhlee95:supercrv95@ds261755.mlab.com:61755/book_graphql", { useNewUrlParser: true })
-      .then(() => console.log('Connected to mLab'))
-      .catch(err => console.log(err));
+mongoose.Promise = global.Promise;
+
+let mongoUserCredentials = '';
+if (process.env.MONGO_USER && process.env.MONGO_PASSWORD) {
+  mongoUserCredentials = `${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@`;
+}
+
+const MONGO_URL = process.env.MONGO_URL || 'localhost:27017';
+const DB_NAME = process.env.MONGO_DB_NAME || 'sample-db';
+const MONGO_CONNECTION_STRING = `mongodb://${mongoUserCredentials}${MONGO_URL}/${DB_NAME}`;
+
+await mongoose.connect(MONGO_CONNECTION_STRING);
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server started on port: ${process.env.PORT || 4000}`);
