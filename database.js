638691eb28096995be85a96cf26888be2643fcc9
Vinhlee95 <lethanhvinh95@gmail.com>
2018-09-11 23:18:06 +0300

    fix

--- database.js
+++ database.js
@@ -6,18 +6,15 @@ const connectToDB = async () => {
   const mongoose = new Mongoose();
   mongoose.Promise = global.Promise;
 
-  // let mongoUserCredentials = '';
-  // if (process.env.MONGO_USER && process.env.MONGO_PASSWORD) {
-  //   mongoUserCredentials = `${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@`;
-  // }
+  let mongoUserCredentials = '';
+  if (process.env.MONGO_USER && process.env.MONGO_PASSWORD) {
+    mongoUserCredentials = `${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@`;
+  }
 
-  // const MONGO_URL = process.env.MONGO_URL || 'localhost:27017';
-  // const DB_NAME = process.env.MONGO_DB_NAME || 'sample-db';
-  // const MONGO_CONNECTION_STRING = `mongodb://${mongoUserCredentials}${MONGO_URL}/${DB_NAME}`;
+  const MONGO_URL = process.env.MONGO_URL || 'localhost:27017';
+  const DB_NAME = process.env.MONGO_DB_NAME || 'sample-db';
+  const MONGO_CONNECTION_STRING = `mongodb://${mongoUserCredentials}${MONGO_URL}/${DB_NAME}`;
 
-  await mongoose.connect('mongodb://vinhlee95:supercrv95@ds261755.mlab.com:61755/book_graphql'), { useNewUrlParser: true };
-  connection = mongoose;
-};
 
 const getDB = () => {
   if (!connection) {
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const schema = require('./schema/schema');
-const mongoose = require('mongoose');
+const { connectToDB } = require('./database');
 
 const app = express();
 schema.applyMiddleware({ app });
 
-mongoose.connect("mongodb://vinhlee95:supercrv95@ds151292.mlab.com:51292/heroku_l4gvpwqf", { useNewUrlParser: true })
-      .then(() => console.log('Connected to mLab'))
-      .catch(err => console.log(err));
+connectToDB():
+
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server started on port: ${process.env.PORT || 4000}`);
