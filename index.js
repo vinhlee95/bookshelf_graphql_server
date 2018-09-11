e02ee33c59676288742ec10ffb61f0c055034d37
Vinhlee95 <lethanhvinh95@gmail.com>
2018-09-12 00:43:38 +0300

    fix server

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const schema = require('./schema/schema');
 const { connectToDB } = require('./database');
+const { ApolloEngine } = require("apollo-engine");
+connectToDB();
 
 const app = express();
 schema.applyMiddleware({ app });
 
-connectToDB();
 
-
-schema.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+const engine = new ApolloEngine({
+  apiKey: "service:vinhlee95-2446:K0i_jMHCuPwARoAAAQ0WkA"
 });
+
+// Call engine.listen instead of app.listen(port)
+engine.listen({
+  port: process.env.PORT || 4000 ,
+  expressApp: app
+});
+
