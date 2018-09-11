24b3bdaa9f1d93d3469c6eb305f6e608558344d6
Vinhlee95 <lethanhvinh95@gmail.com>
2018-09-11 22:48:42 +0300

    fix server

--- index.js
+++ index.js
@@ -5,18 +5,9 @@ const mongoose = require('mongoose');
 const app = express();
 schema.applyMiddleware({ app });
 
-mongoose.Promise = global.Promise;
-
-let mongoUserCredentials = '';
-if (process.env.MONGO_USER && process.env.MONGO_PASSWORD) {
-  mongoUserCredentials = `${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@`;
-}
-
-const MONGO_URL = process.env.MONGO_URL || 'localhost:27017';
-const DB_NAME = process.env.MONGO_DB_NAME || 'sample-db';
-const MONGO_CONNECTION_STRING = `mongodb://${mongoUserCredentials}${MONGO_URL}/${DB_NAME}`;
-
-await mongoose.connect(MONGO_CONNECTION_STRING);
+mongoose.connect("mongodb://vinhlee95:supercrv95@ds151292.mlab.com:51292/heroku_l4gvpwqf", { useNewUrlParser: true })
+      .then(() => console.log('Connected to mLab'))
+      .catch(err => console.log(err));
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server started on port: ${process.env.PORT || 4000}`);
